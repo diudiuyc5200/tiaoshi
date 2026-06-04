86f6b49d1539e184ce5a3816e34488dc0fb32696
diudiuyc5200 <17766728994@163.com>
2026-06-04 16:22:08 +0800

    Set page_cluster to 1 for ZRAM and ZSWAP

--- mm/swap.c
+++ mm/swap.c
@@ -1052,4 +1052,7 @@ void __init swap_setup(void)
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
 	 */
+	#if defined(CONFIG_ZRAM) || defined(CONFIG_ZSWAP)
+	page_cluster = 1;
+#endif
 }
